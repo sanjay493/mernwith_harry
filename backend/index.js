70e941226a6eba95e9266db762c34562f11cf563
Kumar Sanjay <sanjayism@gmail.com>
2022-02-05 02:10:24 +0530

    lesson 45 completed

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 connectToMongo()
 app.use('/api/v1/auth',require('./routes/auth'))
 //app.use('/api/v1/notes',require('./routes/notes'))
