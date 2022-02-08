565f3b5865985cd59e28a5107de3cc95deb0b9e2
Kumar Sanjay <sanjayism@gmail.com>
2022-02-08 22:17:48 +0530

    Lesson 50 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -48,4 +48,40 @@ router.post('/createuser',[
   }
 })
 
+//Login User using : POST "/api/auth/login" . Doesn't require Authentication
+router.post('/login',[
+ body('email', 'Enter a valid Email').isEmail().toLowerCase(),
+  body('password','password can not be empty').exists(),
+],async(req,res)=>{
+ // If there are errors, return Bad request and errors
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() });
+ }
+const {email,password}=req.body
+try{
+    // check whether the email exit
+    //search email from exiting database
+let user=await User.findOne({email})
+if(!user){
+    return res.status(400).json({error:"Enter a valid Credentails"})
+} 
+//create a secure password with bcrypt which is async function
+const passwordCompare=await bcrypt.compare(password,user.password)
+if(!passwordCompare){
+  return res.status(400).json({error:"Enter a valid Credentails"})
+} 
+const data={
+    id:user.id
+}
+const authToken=jwt.sign(data,JWT_SECRET)
+
+res.json(authToken)
+}catch (error){
+    console.error(error.message)
+    res.status(500).send("Some error occurred")
+}
+})
+
 module.exports=router
