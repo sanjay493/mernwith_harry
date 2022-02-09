fff471d7f9a5686d0bf98bc2190f91a0a62ed71c
Kumar Sanjay <sanjayism@gmail.com>
2022-02-09 23:25:01 +0530

    Lesson 54 completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -65,6 +65,28 @@ router.put('/updatenote/:id',fetchuser,async(req,res)=>{
   res.json(note)
 })
 
+
+//ROUTE 4: Delete a Note using : DELETE "/api/auth/deletenote" . Login required
+router.delete('/deletenote/:id',fetchuser,async(req,res)=>{
+        
+    try {
+        //find the note to be deleted
+    let note=await Note.findById(req.params.id)
+    if(!note){res.status(404).send("Not Found")}
+    //Allow deletion only if user owns this note
+    if(note.user.toString()!==req.user.id){
+        return res.status(401).send("Not Allowed")
+    }
+
+    note=await Note.findByIdAndDelete(req.params.id)
+    res.json({"success":"Note has been deleted",note:note})
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error occurred")
+   
+    }
+    
+  })
     
 
 module.exports=router
